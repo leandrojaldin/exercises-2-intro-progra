6d6e3e2a87563ba24286e18228a07ea055f88579
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-04-02 15:33:14 -0400

    1

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -6,16 +6,12 @@ using namespace std;
 
 void exercise_1(string s1) {
   // TODO: YOUR CODE HERE
-  for (int i = 0; i <= s1.size(); ++i) {
-    if(s1[i] == ' ' || i == s1.size()) {
-      if (!word.empty()) {
-        cout << "[" << word << "]" << endl;
-        word = "";
-      }
-    } else {
-      word += s1[i]
-    }
-  }
+     for (int i = 0; i <= s1.size(); ++i) {
+          if(s1[i] == ' '){
+            cout << i << endl;
+          }
+     }
+     cout << s1.size() << endl; 
 }
 
 void exercise_2(string s1) {
