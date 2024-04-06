e3a9ea1997d66f05cb54905d2099b293b53e3129
leandrojaldin <160991852+leandrojaldin@users.noreply.github.com>
2024-04-05 23:23:54 -0400

    2

--- src/exercises.cpp
+++ src/exercises.cpp
@@ -16,6 +16,18 @@ void exercise_1(string s1) {
 
 void exercise_2(string s1) {
   // TODO: YOUR CODE HERE
+    string word = "";
+
+    for (int i = 0; i < s1.size(); ++i) {
+        if (s1[i] != ' ') {
+            word += s1[i];
+        } else {
+            if (word != "") {
+                cout << "[" << word << "]" << endl;
+                word = "";
+            }
+        }
+    }
 }
 
 void exercise_3(string s1) {
